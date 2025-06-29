ccd883b56f2bcf3c14640834bbdf3d080e61d3e0
anaC-Fernandes <ana.clementino@ufv.br>
2025-06-29 18:42:15 -0300

    Remove some ununsed files

--- src/teste-apagar-dps/match/testeAPI.js
+++
@@ -1,9 +0,0 @@
-// Caminho correto, sobe um nível de pasta
-import { buscarFilmePorNome } from '../api.js';
-
-async function main() {
-  const filme = await buscarFilmePorNome("Pulp Fiction: Tempo de Violência");
-  console.log(filme);
-}
-
-main();
--- src/teste-apagar-dps/testeAPI.js
+++
@@ -1,8 +0,0 @@
-import { buscarFilmePorNome } from './api.js';
-
-async function main() {
-  const filme = await buscarFilmePorNome("Pulp Fiction: Tempo de Violência");
-  console.log(filme);
-}
-
-main();
